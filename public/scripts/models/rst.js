f89d19526c9bc27af21d3664fd8e965a9d591787
ayanle <ayanle.abdi@gmail.com>
2018-01-17 21:32:21 -0800

    getting some constructors started

---
+++ public/scripts/models/rst.js
@@ -0,0 +1,21 @@
+Neighborhood.all = [];
+
+// location
+function Neighborhood(data){
+    this.title = data.title;
+    this.city_id = data.city_id;
+    this.latitude = data.latitude;
+    this.longitude = data.longitude;
+    this.city_name = data.city_name;
+}
+
+
+// popularity
+function Popularity(data){
+    
+}
+
+// nearbyRes
+function NearbyRes(data){
+    
+}
