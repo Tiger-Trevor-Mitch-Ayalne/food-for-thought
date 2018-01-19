09bfa87402699f860ccf3ea67cb0372af69f2f53
Mitchel Hall <scubaphoto@gmail.com>
2018-01-18 21:19:49 -0800

    Factored code that now shows limited location data on the page. Have some ideas for how to get the rest of the data to show.

--- public/scripts/models/rst.js
+++ public/scripts/models/rst.js
@@ -2,6 +2,7 @@ Neighborhood.all = [];
 Popularity.all = [];
 NearbyRes.all = [];
 
+let restaurants  ;
 // location
 function Neighborhood(data){
     this.title = data.title;
@@ -22,3 +23,10 @@ function Popularity(data){
 function NearbyRes(data){
     Object.keys(data).forEach(key => this[key] = data[key]);   
 }
+
+
+    NearbyRes.prototype.toHtml = function() {
+        let template = Handlebars.compile($('#restaurant-template').text());
+        return template(this);
+    }
+  
