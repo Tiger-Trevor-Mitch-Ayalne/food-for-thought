8562e80bfb5314edafc8793601a33566d4a46ad0
ayanle <ayanle.abdi@gmail.com>
2018-01-22 18:22:15 -0800

    dynamic views

---
+++ public/scripts/views/navigation/main-menu-view.js
@@ -0,0 +1,29 @@
+var app = app || {};
+(module => {
+
+    const MainMenuView = {};
+
+    const markup = `
+        <ul id="simple-nav-menu">
+            <li data-nav_id="home"><a href="/">Home</a></li>
+            <li data-nav_id="about"><a href="/about-us-view">About Us</a></li>
+            <li data-nav_id="contact"><a href="/contact-us-view">Contact Us</a></li>
+            <li data-nav_id="register"><a href="/register-view">Register</a></li>
+            <li data-nav_id="login"><a href="/login-view">Login</a></li>
+            <li data-nav_id="admin"><a href="/admin-view">Admin</a></li>
+        </ul>
+    `
+    const template = Handlebars.compile(markup)
+
+    function renderThings() {
+        $('#main-menu-slot').empty()
+        $('#main-menu-slot').append(template())
+    }
+    MainMenuView.init = () => {
+        
+        $('#main-menu-view').off()
+        renderThings()
+        $('#main-menu-view').show()
+    }
+    module.MainMenuView = MainMenuView
+})(app)
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 app.get('/api/v2.1/geocode', (req, res) => {
-    const url = `https://developers.zomato.com/api/v2.1/geocode?${req.query.lat}&lon=${req.query.long}`;
+  //https://developers.zomato.com/api/v2.1/geocode?lat=47.5324723999&lon=-122.2721882
+
+    const url = `https://developers.zomato.com/api/v2.1/geocode?lat=${req.query.lat}&lon=${req.query.long}`;
     console.log('new url',url)
     superagent.get(url)
       .set(`user-key`, `c432c4bb526c687aabed6e596d23735f`)
@@ -24,6 +26,18 @@ app.get('/api/v2.1/geocode', (req, res) => {
         err => res.send(err)
       )
   })
+//https://developers.zomato.com/api/v2.1/reviews?res_id=16726738&count=100
+app.get('/api/v2.1/reviews', (req, res) => {
+    const url = `https://developers.zomato.com/api/v2.1/reviews?res_id=${req.query.id}&count=100`;
+    console.log('new url',url)
+    superagent.get(url)
+      .set(`user-key`, `c432c4bb526c687aabed6e596d23735f`)
+      .then(
+        data => res.send(data.text),
+        err => res.send(err)
+      )
+  })
+
 
 app.get('/*', cors(),(request, response) => {
     response.sendFile('index.html', { root: './public' });
