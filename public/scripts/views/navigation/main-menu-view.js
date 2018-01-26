cbbda1e673c64bb63dbe37eb8666c6f41d9843c6
playing_tygre <kymiehsu123@gmail.com>
2018-01-25 18:25:37 -0800

    hamburger

--- public/scripts/views/navigation/main-menu-view.js
+++ public/scripts/views/navigation/main-menu-view.js
@@ -27,3 +27,23 @@ var app = app || {};
     }
     module.MainMenuView = MainMenuView
 })(app)
+
+$( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+    
+    });
